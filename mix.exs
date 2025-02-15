1e6a140f1bd8075d6e5ac6c850d5fead0d6f46ef
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2025-02-15 17:30:59 +0000

    Update websockex dependency

--- mix.exs
+++ mix.exs
@@ -29,13 +29,7 @@ defmodule WebsocketSyncClient.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {
-        # the version currently in hex.pm is 0.4.3, which doesn't have a change regarding correct
-        # handling of stack traces, so grab the commit at master where it's fixed
-        :websockex,
-        git: "https://github.com/Azolo/websockex.git",
-        tag: "4a94f6870528f45d64cdd47bd4374faf52528466"
-      },
+      {:websockex, "~> 0.5.0", hex: :websockex_wt},
       {:cowboy, "~> 2.9", only: :test},
       {:plug_cowboy, "~> 2.5", only: :test},
       {:plug, "~> 1.4", only: :test},
